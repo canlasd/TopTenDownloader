fafa72e96b91a77084975aaec8944495bd77c708
canlasd <[canlasd@aol.com]>
2016-05-20 14:16:27 -0400

    Initial Commit.  Establish communication with URL using async task

---
+++ app/src/main/java/canlasd/example/com/top10downloader/MainActivity.java
@@ -0,0 +1,102 @@
+package canlasd.example.com.top10downloader;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String
+            URL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws" +
+            "/RSS/topfreeapplications/limit=10/xml";
+
+    private TextView xmlTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xmlTextview = (TextView) findViewById(R.id.xmlTextview);
+
+        DownloadData download = new DownloadData();
+        download.execute(URL);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXmlFile(params[0]);
+
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error downloading data");
+            }
+
+            return mFileContents;
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d("DownloadData", "Result: " + result);
+            xmlTextview.setText(mFileContents);
+        }
+
+        private String downloadXmlFile(String urlFile) {
+            StringBuilder stringBuffer = new StringBuilder();
+            try {
+
+                URL url = new URL(urlFile);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", " Response Code: " + response);
+
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+                char[] inputBuffer = new char[500];
+
+                while (true) {
+                    charRead = isr.read(inputBuffer);
+
+                    if (charRead <= 0) {
+                        break;
+                    }
+
+                    stringBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+                }
+
+                return stringBuffer.toString();
+
+
+            } catch (IOException e) {
+                Log.d("DownloadData", "Error Message " + e.getMessage());
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                Log.d("DownloadData", "Security Exception: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+
+            // return null if there is an exception found
+            return null;
+        }
+
+    }
+
+}
